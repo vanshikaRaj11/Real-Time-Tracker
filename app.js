0b0ccb226bc950094834414735a0b4ce538b47a8
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-16 23:01:39 +0530

    Added folders, added app.js code

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+app.set("view engine", "ejs")
+app.set(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.send("Hello, I am real time tracker app");
+});
+
+server.listen(3000);
