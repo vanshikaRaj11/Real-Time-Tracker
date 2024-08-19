9e20e703748211a2d476b7e724d44325376d932b
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-19 21:34:57 +0530

    Connection issue resolved

--- app.js
+++ app.js
@@ -7,12 +7,21 @@ const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine", "ejs")
-app.set(express.static(path.join(__dirname, "public")));
+io.on("connection", function (socket) {
+  console.log("Connected");
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });  
+});
 
 app.get("/", function (req, res) {
-  res.send("Hello, I am real time tracker app");
+  res.render("index");
 });
 
 server.listen(3000);
