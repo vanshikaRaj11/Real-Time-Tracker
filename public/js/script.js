78fba2363f18eab4716d0baa84189f346c10a49c
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-24 16:51:10 +0530

    Everything fixed

--- public/js/script.js
+++ public/js/script.js
@@ -17,28 +17,27 @@ if (navigator.geolocation) {
   );
 }
 
-const map = L.map("map").setView([0, 0], 16);
+const map = L.map("map").setView([0, 0], 16); //L.map is used for asking permission to allow location
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attibution:"OpenStreetMap"
-}).addTo(map)
+  attibution: "OpenStreetMap",
+}).addTo(map);
 
 const markers = {};
 
 socket.on("receive-location", (data) => {
-    const { id, latitude, longitude } = data;
-    map.setView([latitude, longitude])
-    if (markers[id]) {
-        markers[id].setLatLng([latitude,longitude])
-    } else {
-        markers[id] = L.marker([latitude,longitude]).addTo(map)
-    }
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
 });
 
 socket.on("user-disconnected", (id) => {
-    if (markers[id]) {
-        map.removeLayer(markers[id])
-        delete markers[id]
-    }
-})
-
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
